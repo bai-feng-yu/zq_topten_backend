bb0a8ef6e65f5000c18351e07a69beb094b75ac1
dreagonm <1748281467@qq.com>
2022-03-28 23:24:33 +0800

    [FIX] 修正路由

--- zq_topten_backend/vote/urls.py
+++ zq_topten_backend/vote/urls.py
@@ -3,14 +3,14 @@ from . import views
 
 urlpatterns = [
     re_path('^vote/$',views.VoteView.as_view()),
-    re_path('^Candidate/$',views.CandidateView.as_view({'get':'list'})),
-    re_path('^Candidate/(?P<id>\d+)/$',views.CandidateView.as_view({'get':'retrieve'})),
-    re_path('^Announcement/$',views.AnnoncementsView.as_view({'get':'list'})),
+    re_path('^candidate/$',views.CandidateView.as_view({'get':'list'})),
+    re_path('^candidate/(?P<id>\d+)/$',views.CandidateView.as_view({'get':'retrieve'})),
+    re_path('^announcement/$',views.AnnoncementsView.as_view({'get':'list'})),
     re_path('^import/$',views.ImportView.as_view()),
-    re_path('^Photo/(?P<id>\d+)/$',views.PhotoView.as_view()),
-    re_path('^HistoryPhoto/(?P<id>\d+)/$',views.HistoryPhotoView.as_view()),
-    re_path('^History/$',views.HistoryView.as_view()),
-    re_path('^Votestatus/$',views.VotestatusView.as_view()),
-    re_path('^Captcha/$',views.CaptchaView.as_view()),
+    re_path('^photo/(?P<id>\d+)/$',views.PhotoView.as_view()),
+    re_path('^historyphoto/(?P<id>\d+)/$',views.HistoryPhotoView.as_view()),
+    re_path('^history/$',views.HistoryView.as_view()),
+    re_path('^votestatus/$',views.VotestatusView.as_view()),
+    re_path('^captcha/$',views.CaptchaView.as_view()),
     re_path('',views.IndexView.as_view())
 ]
