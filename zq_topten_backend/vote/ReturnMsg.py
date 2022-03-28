6c2c6b9df5abd8bb3a1381ba06295d9bd073e12d
dreagonm <1748281467@qq.com>
2022-03-28 10:26:08 +0800

    [Update] 修改返回信息

--- zq_topten_backend/vote/ReturnMsg.py
+++ zq_topten_backend/vote/ReturnMsg.py
@@ -2,26 +2,25 @@ class ReturnMsg(object):
     _Code = 200
     _Msg = 'Ok'
     _Data = None
-    
-    def __init__(self,Code = None,Msg = None,Data = None):
+    _kwdata = None
+
+    def __init__(self,Code = None,Msg = None,Data = None,**kwargs):
         if Code:
             self._Code  = Code
         if Msg:
             self._Msg = Msg
-    
-    def Update(self,Code = None,Msg = None,Data = None):
-        if Code:
-            self._Code  = Code
-        if Msg:
-            self._Msg = Msg
+          
+        self._kwdata = kwargs
 
     @property
     def Data(self):
-        return {
+        ret = {
             'Code' : self._Code,
             'Msg' : self._Msg,
             'Data' : [] if self._Data == None else self._Data
         }
+        ret.update(self._kwdata)
+        return ret
 
     
     
