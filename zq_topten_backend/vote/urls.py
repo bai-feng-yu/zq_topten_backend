e28463c9fa79dfd9882caddce411d9a0f88e8069
dreagonm <1748281467@qq.com>
2022-03-28 16:26:01 +0800

    [FIX] 修正路由错误

--- zq_topten_backend/vote/urls.py
+++ zq_topten_backend/vote/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     re_path('^import/$',views.ImportView.as_view()),
     re_path('^Photo/(?P<id>\d+)/$',views.PhotoView.as_view()),
     re_path('^HistoryPhoto/(?P<id>\d+)/$',views.HistoryPhotoView.as_view()),
-    re_path('^HistoryView/$',views.HistoryView.as_view()),
+    re_path('^History/$',views.HistoryView.as_view()),
     re_path('^Votestatus/$',views.VotestatusView.as_view()),
     re_path('',views.IndexView.as_view())
 ]
